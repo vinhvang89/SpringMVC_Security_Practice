7d61fd7983e757429080d8e105299555ca9f1d83
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-07-03 09:39:05 +0700

    new

---
+++ src/main/java/com/WebSecInit.java
@@ -0,0 +1,6 @@
+package com;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecInit extends AbstractSecurityWebApplicationInitializer {
+}
